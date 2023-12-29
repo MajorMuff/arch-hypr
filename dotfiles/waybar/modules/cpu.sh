3fbcf127181052a66fed2a7f299e5cd38241fa9b
MajorMuff <c.diepenbroek@gmail.com>
2023-12-29 19:20:02 +0100

    add boot function to mainctl

--- dotfiles/waybar/modules/cpu.sh
+++ dotfiles/waybar/modules/cpu.sh
@@ -13,7 +13,7 @@ module_cpu() {
 
     idle=$(mpstat 2 1 | tail -1 | awk '{print $NF}')
     printf -v usage "%.1f%%" "$(bc -l <<< "100 - $idle")"
-    tooltip="CPU Model: $(grep "model name" /proc/cpuinfo | head -1 | cut -d: -f2 | xargs | cut -d' ' -f 1-4)"
+    tooltip="CPU Model: $(grep "model name" /proc/cpuinfo | head -1 | awk '{ for(i=4;i<=7;i++) printf "%s ", $i }')"
 
     jq -nc \
         --arg text "$usage" \
